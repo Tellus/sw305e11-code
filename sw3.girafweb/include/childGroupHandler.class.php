1ad7bbf7ce0a889eccfbc632560573a25016c70f
Lisbeth <lniel10@student.aau.dk>
2011-11-18 14:32:20 +0100

    ændret navne
    Signed-off-by: Lisbeth <lniel10@student.aau.dk>

--- sw3.girafweb/include/childGroupHandler.class.php
+++ sw3.girafweb/include/childGroupHandler.class.php
@@ -12,25 +12,30 @@ class childGroups
 	private $intersectionApps;
 	
 	/**
-	* init the class using a group ID
-	* \param a group ID
+	* 
 	*/
 	public function __construct($gID)
 	{
 		$this->id = $gID;
 		$this->group = GirafGroup::getGirafGroup($gID);
-		$this->children = GirafGroup::getChildrenInGroup($gID);
+		getChildrenFromDB();
 	}
 	
 	
 	//--------------getters-----------------\\
-	/**
-	*\return the group ID
-	*/
 	public function getGroupId()
 	{
 		return $this->id;
 	}
+	
+	private function getChildrenFromDB()
+	{
+		$childrenIDs = GirafGroup::getChildrenInGroup($this->id);
+		foreach($children as $value)
+		{
+			$this->children[$value]= childProfile($value);
+		}
+	}	
 	
 
 	
