7f818e69b96ff956d41d01543c1175ab291afeb7
Lisbeth <lniel10@student.aau.dk>
2011-11-21 11:40:28 +0100

    små ændringer

--- sw3.girafweb/include/DeviceHandler.class.php
+++ sw3.girafweb/include/DeviceHandler.class.php
@@ -2,11 +2,13 @@
 require_once(__DIR__ . "/device.class.inc");
 class DeviceHandler
 {
+	private $id;
 	private $device;
 	
 	public function __construct($ID)
 	{
 		$this->device = GirafDevices::getDevice($ID);
+		$this->id = $ID; 
 	}
 	
 	
--- sw3.girafweb/include/applicationHandler.class.php
+++ sw3.girafweb/include/applicationHandler.class.php
@@ -3,7 +3,14 @@ require_once(__DIR__ . "/applications.class.inc");
 
 class applicationHandler
 {
-
+	private id;
+	private application;
+	
+	public function __construct($ID)
+	{
+		$this->application = GirafApplications::getDevicesWithInstalledApp($ID);
+		$this->id = $ID;
+	}
 } 
 
 
